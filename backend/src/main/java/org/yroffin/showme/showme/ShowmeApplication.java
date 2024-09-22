78b9d61e0860bb8183276a5c3ef0c65178d97690
Yannick Roffin <yroffin@gmail.com>
2024-09-22 14:52:16 +0200

    feat(init): :tada: init backend

---
+++ backend/src/main/java/org/yroffin/showme/showme/ShowmeApplication.java
@@ -0,0 +1,13 @@
+package org.yroffin.showme.showme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowmeApplication.class, args);
+	}
+
+}
